fcb0ea9d5f0c938b5ae87bf2f41d609a79399a7e
Tanguygab <tanguy@gabril.com>
2026-01-01 18:28:58 +0100

    Fixed player heads

--- src/main/kotlin/io/tanguygab/yarmm/config/menu/meta/SkullMetaConfig.kt
+++ src/main/kotlin/io/tanguygab/yarmm/config/menu/meta/SkullMetaConfig.kt
@@ -1,7 +1,7 @@
 package io.tanguygab.yarmm.config.menu.meta
 
-import com.google.gson.Gson
 import com.google.gson.JsonObject
+import io.tanguygab.yarmm.YARMM
 import io.tanguygab.yarmm.config.menu.ItemMetaConfig
 import io.tanguygab.yarmm.inventory.MenuItemView
 import me.neznamy.tab.shared.Property
@@ -10,6 +10,7 @@ import me.neznamy.tab.shared.platform.TabPlayer
 import org.bukkit.Bukkit
 import org.bukkit.inventory.meta.ItemMeta
 import org.bukkit.inventory.meta.SkullMeta
+import java.net.URI
 import java.util.UUID
 import kotlin.io.encoding.Base64
 
@@ -19,10 +20,11 @@ class SkullMetaConfig(val head: String) : ItemMetaConfig(SkullMeta::class) {
     )
 
     override fun refresh(meta: ItemMeta, data: Map<String, Property>, force: Boolean) {
+        meta as SkullMeta
+
         val head = data["head"]!!
         if (head.update() || force) {
-            val skin = TAB.getInstance().configuration.skinManager.getSkin(head.get())
-            meta as SkullMeta
+            val skin = TAB.getInstance().configuration.skinManager.getSkin(head.get().trim())
             if (skin == null) {
                 meta.playerProfile = null
                 return
@@ -31,28 +33,16 @@ class SkullMetaConfig(val head: String) : ItemMetaConfig(SkullMeta::class) {
             if (meta.playerProfile == null)
                 meta.playerProfile = Bukkit.createProfile(UUID.randomUUID())
 
-            val textures = gson.fromJson(Base64.decode(skin.value).toString(Charsets.UTF_8), JsonObject::class.java)
+            val textures = YARMM.gson.fromJson(Base64.decode(skin.value).toString(Charsets.UTF_8), JsonObject::class.java)
             val url = textures["textures"]?.asJsonObject["SKIN"]?.asJsonObject["url"]?.asString
 
-            // broken, need to fix
-//            meta.playerProfile!!.apply {
-//                this.textures.skin = if (url == null) null else URI.create(url).toURL()
-//                update().thenAccept {
-//                    println(it.isComplete)
-//                    meta.playerProfile = it
-//                }
-//                meta.playerProfile = update().get()
-//                println(meta.playerProfile!!.isComplete)
-//                meta.playerProfile = update().join()
-//                println(meta.playerProfile!!.isComplete)
-//                println(isComplete)
-//                println(complete())
-//                println(isComplete)
-//            }
-        }
-    }
+            val profile = meta.playerProfile!!
+            profile.setTextures(profile.textures.apply {
+                this.skin = if (url == null) null else URI.create(url).toURL()
+            })
 
-    companion object {
-        private val gson = Gson()
+            // blocking when skin not found, should find an alternative soon
+            meta.playerProfile = profile
+        }
     }
 }
