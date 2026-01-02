886de2712328b3da44433a1746c8ce1b0e1ccddc
Tanguygab <tanguy@gabril.com>
2026-01-02 18:03:06 +0100

    Fixed expired prompts not removed from map & allowed prompts outside of menus

--- src/main/kotlin/io/tanguygab/yarmm/listeners/PlayerListener.kt
+++ src/main/kotlin/io/tanguygab/yarmm/listeners/PlayerListener.kt
@@ -7,6 +7,7 @@ import org.bukkit.event.EventHandler
 import org.bukkit.event.EventPriority
 import org.bukkit.event.Listener
 import org.bukkit.event.player.AsyncPlayerChatEvent
+import org.bukkit.event.player.PlayerQuitEvent
 import java.util.concurrent.CompletableFuture
 import java.util.concurrent.TimeUnit
 
@@ -20,19 +21,28 @@ class PlayerListener(val plugin: YARMM) : Listener {
         return try {
             if (cooldown == -1L) future.get()
             else future.get(cooldown, TimeUnit.MILLISECONDS)
-        } catch (_: Exception) { null }
+        } catch (_: Exception) {
+            prompts.remove(player)
+            null
+        }
     }
 
     @EventHandler(priority = EventPriority.LOW, ignoreCancelled = true)
     fun onChat(@Suppress("DEPRECATION") e: AsyncPlayerChatEvent) {
         val player = e.player.tab
-        if (player !in plugin.menuManager.sessions) return
 
-        e.isCancelled = true
+        if (player in plugin.menuManager.sessions) e.isCancelled = true
         if (player!! !in prompts) return
+        e.isCancelled = true
 
         prompts[player]!!.complete(e.message)
         prompts.remove(player)
     }
 
+    @EventHandler
+    fun onLeave(e: PlayerQuitEvent) {
+        val player = e.player.tab ?: return
+        prompts.remove(player)
+        plugin.menuManager.sessions.remove(player)
+    }
 }
